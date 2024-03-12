8e6392a765e669142f8c5dd22c73e85cd4818c51
SvyatoslavShatilov <aiblu101@gmail.com>
2024-03-12 21:07:40 +0300

    добавили новый вид задачи цикла, добавили комент и способ написания в строку, добавили к нему способ написания в строку с пробелом

---
+++ OneDrive/Рабочий стол/C#/lesson1/Task5/Program.cs
@@ -0,0 +1,29 @@
+﻿int n = 100;
+int i = 1;
+
+while (i <= n)
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
+
+int n1 = 100;
+int i1 = 1;
+
+while (i1 <= n1)
+{
+    //Console.WriteLine(i1);
+    Console.Write(i1);
+    i1 = i1 + 1;
+}
+
+int n2 = 100;
+int i2 = 1;
+
+while (i2 <= n2)
+{
+    //Console.WriteLine(i2);
+    Console.Write(i2);
+    Console.Write(' ');
+    i2 = i2 + 1;
+}
